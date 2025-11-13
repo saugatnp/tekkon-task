1388e699973601e2a28b08042239b14dcf556458
saugatnp <saugatnp11@gmail.com>
2025-11-13 07:29:02 +0545

    feat: Added json formatting option

--- src/app/components/json-editor/json-editor.ts
+++ src/app/components/json-editor/json-editor.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, model, output, signal } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, model, output, signal} from '@angular/core';
+import {JsonUtils} from '../../utils/json.utils';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-json-editor',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './json-editor.html',
   styleUrls: ['./json-editor.scss']
 })
@@ -13,8 +16,22 @@ export class JsonEditor {
   jsonContent = model<string>('');
   jsonChange = output<string>();
 
+  readonly isValidJson = computed((): boolean => {
+    const result = JsonUtils.parse(this.jsonContent());
+    return result.success;
+  });
+
   onInput(value: string) {
     this.jsonContent.set(value);
     this.jsonChange.emit(value);
+  }
+
+  formatJson(): void {
+    const result = JsonUtils.parse(this.jsonContent());
+    if (result.success) {
+      const formatted = JsonUtils.stringify(result.data);
+      this.jsonContent.set(formatted);
+      this.jsonChange.emit(formatted);
+    }
   }
 }
