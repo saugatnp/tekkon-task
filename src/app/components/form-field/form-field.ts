aa7fa5a8935177f16c75f74fd6d141c56a7ac32d
saugatnp <saugatnp11@gmail.com>
2025-11-13 07:38:56 +0545

    feat: Initialize basic form fields

--- src/app/components/form-field/form-field.ts
+++ src/app/components/form-field/form-field.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, computed, input} from '@angular/core';
+import {AbstractControl, FormControl, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-form-field',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './form-field.html',
   styleUrl: './form-field.scss',
 })
 export class FormField {
+
+  readonly label = input<string>('');
+  public control = input.required<AbstractControl>();
+  readonly type = input<'text' | 'textarea' | 'number'>('text');
+  readonly placeholder = input<string>('');
+
+  readonly formControl = computed((): FormControl => this.control() as FormControl);
+
+
+  constructor() {
+
+  }
+
+
 
 }
